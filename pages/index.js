6fa7177071ff8433cb170ccf5de7d36e7c3a79f6
George Brown <teawares@gmail.com>
2020-11-26 16:03:27 +0800

    ✨

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,33 @@
-import Nav from '../components/nav'
+import Head from "next/head";
+import { motion, useViewportScroll, useTransform } from "framer-motion";
 
 export default function IndexPage() {
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
   return (
-    <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Next.js + Tailwind CSS 2.0
-        </h1>
+    <>
+      <Head>
+        <title>感恩節</title>
+      </Head>
+      <div className="w-screen h-screen-3 overflow-x-hidden bg-gradient-to-b from-purple-400 via-red-400 to-yellow-500">
+        <div className="w-36 h-36 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <motion.div
+            className="w-full h-full flex justify-center items-center overflow-hidden bg-white rounded-xl"
+            style={{
+              scale,
+            }}
+          >
+            <motion.div
+              className=""
+              style={{
+                scale: useTransform(scrollYProgress, [0, 1], [0, 4]),
+              }}
+            >
+              🦃️
+            </motion.div>
+          </motion.div>
+        </div>
       </div>
-    </div>
-  )
+    </>
+  );
 }
